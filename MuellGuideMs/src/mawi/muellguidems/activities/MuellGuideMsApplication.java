666b9ab826f9c333448b7a022c96962a9390b7fb
marlen.rotert@gmail.com <marlen.rotert@gmail.com@494df4b8-804f-c712-43b1-10418e7df321>
2014-12-11 18:52:03 +0000

    Einrichtung des Projekts und Parse Test

---
+++ MuellGuideMs/src/mawi/muellguidems/activities/MuellGuideMsApplication.java
@@ -0,0 +1,32 @@
+package mawi.muellguidems.activities;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseCrashReporting;
+import com.parse.ParseUser;
+
+public class MuellGuideMsApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		// Initialize Crash Reporting.
+		ParseCrashReporting.enable(this);
+
+		// Add your initialization code here
+		Parse.initialize(this, "bRJ7kGmcbXI36Ft0kHL3u2G3KGKV5aVK8kwjjZPM",
+				"ZLY3xn8IXUVSBUP25DN1cgtwMlUWnPlvPovYkmCP");
+
+		ParseUser.enableAutomaticUser();
+		ParseACL defaultACL = new ParseACL();
+
+		// If you would like all objects to be private by default, remove this
+		// line.
+		defaultACL.setPublicReadAccess(true);
+
+		ParseACL.setDefaultACL(defaultACL, true);
+	}
+}
