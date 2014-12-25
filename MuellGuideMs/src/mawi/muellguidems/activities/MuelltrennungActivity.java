29e235ad87bea50688abce1ff2a6fdbf43ddd21d
ruslanwidemann@googlemail.com <ruslanwidemann@googlemail.com@494df4b8-804f-c712-43b1-10418e7df321>
2014-12-25 12:11:02 +0000

    Custom-Adapter für Hauptmenü-Listview erstellt AdapterSingleItem-Klasse erstellt Erstellung der Hauptmenü-Items in die DAO-Klasse ausgelagert Layout des Hauptmenü-Listviews angepasst (für item-spezifische Icons!) CustomExpandableAdapter umbenannt

--- MuellGuideMs/src/mawi/muellguidems/activities/MuelltrennungActivity.java
+++ MuellGuideMs/src/mawi/muellguidems/activities/MuelltrennungActivity.java
@@ -3,12 +3,10 @@ package mawi.muellguidems.activities;
 import java.util.ArrayList;
 
 import mawi.muellguidems.adapter.AdapterGroupItem;
-import mawi.muellguidems.adapter.CustomExpandableAdapter;
+import mawi.muellguidems.adapter.CustomMuelltrennungExpandableAdapter;
 import mawi.muellguidems.util.DAO;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ExpandableListView;
 
 public class MuelltrennungActivity extends Activity {
@@ -24,31 +22,16 @@ public class MuelltrennungActivity extends Activity {
 		setList();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.muelltrennung, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
 	public void setList() {
 
 		ArrayList<AdapterGroupItem> data = DAO
 				.getAlleGegenstaendeFuerExpandableAdapter();
-		CustomExpandableAdapter adapter = new CustomExpandableAdapter(this,
-				data);
+
+		CustomMuelltrennungExpandableAdapter adapter = null;
+
+		if (data != null) {
+			adapter = new CustomMuelltrennungExpandableAdapter(this, data);
+		}
 
 		elvMuelltrennung.setAdapter(adapter);
 
