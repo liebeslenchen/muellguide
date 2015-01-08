38980eb97a8f00842129a4d36bc2c340f73b5b16
ruslanwidemann@googlemail.com <ruslanwidemann@googlemail.com@494df4b8-804f-c712-43b1-10418e7df321>
2015-01-08 18:09:02 +0000

    Ladebalken in 'Mülltrennung' läuft jetzt flüssig

--- trunk/MuellGuideMs/src/mawi/muellguidems/activities/BaseActivity.java
+++ trunk/MuellGuideMs/src/mawi/muellguidems/activities/BaseActivity.java
@@ -7,11 +7,25 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.widget.Toast;
 
+/**
+ * Diese Klasse dient als Basis-Klasse für die anderen Activities und soll Logik
+ * implementieren, die für alle oder den Großteil aller Activities gilt (z.B.
+ * Netzwerkprüfung). Dadurch soll Mehraufwand bzw. redundanter Code vermieden
+ * werden.
+ */
 public class BaseActivity extends Activity {
 
 	private final NetworkChangedBroadcastReceiver broadcastReceiver = new NetworkChangedBroadcastReceiver();
+
+	// Aktueller Netzwerk-Status
 	private NetworkCondition currentNetworkType;
-	private boolean currentNetworkIsWifi;
+
+	/**
+	 * Gibt an, ob der Toast für langsame Internetverbindung bereits einmal
+	 * angezeigt wurde. Falls ja, soll dieser nicht nochmal angezeigt werden,
+	 * bis zwischendurch eine schnellere Verbindung aktiv war.
+	 */
+	private boolean toastForSlowConnectionAlreadyShown;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +41,8 @@ public class BaseActivity extends Activity {
 		IntentFilter intentFilter = new IntentFilter();
 		registerReceiver(broadcastReceiver, intentFilter);
 		currentNetworkType = MuellGuideMsApplication.getNetzwerkStatus();
-
 		showToastIfNecessary(currentNetworkType);
+
 	}
 
 	/**
@@ -38,21 +52,28 @@ public class BaseActivity extends Activity {
 	 * @param type
 	 */
 	protected void showToastIfNecessary(NetworkCondition type) {
+
+		if (toastForSlowConnectionAlreadyShown
+				&& type == NetworkCondition.SLOW_MOBILE)
+			return;
+
 		switch (type) {
 		case NO_CONNECTION:
 			Toast.makeText(
 					getBaseContext(),
 					"Achtung! Es besteht momentan KEINE  Netzwerkverbindung! Die App kann nicht ohne eine bestehende Verbindung ausgeführt werden!",
 					Toast.LENGTH_LONG).show();
+			toastForSlowConnectionAlreadyShown = false;
 			break;
 		case SLOW_MOBILE:
 			Toast.makeText(
 					getBaseContext(),
 					"Achtung! Ihre Netzwerkverbindung ist momentan sehr langsam. Dadurch werden einige Daten u.U. langsamer geladen!",
 					Toast.LENGTH_LONG).show();
+			toastForSlowConnectionAlreadyShown = true;
 		default:
+			toastForSlowConnectionAlreadyShown = false;
 			break;
 		}
 	}
-
 }
