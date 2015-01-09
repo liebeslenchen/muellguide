bc75ce2810c4e9f7ade1ccd8f40b685aae2c6d06
Rother.Lena@gmail.com <Rother.Lena@gmail.com@494df4b8-804f-c712-43b1-10418e7df321>
2015-01-09 14:08:33 +0000

    Hilfe-Activity

---
+++ trunk/MuellGuideMs/src/mawi/muellguidems/activities/HilfeActivity.java
@@ -0,0 +1,16 @@
+package mawi.muellguidems.activities;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+
+public class HilfeActivity extends BaseActivity {
+
+	Spinner spinner;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_hilfe);
+
+	}
+}
