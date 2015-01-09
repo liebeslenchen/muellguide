d070de580dff65bd647c0160c9bb8b1db17750b9
marlen.rotert@gmail.com <marlen.rotert@gmail.com@494df4b8-804f-c712-43b1-10418e7df321>
2015-01-09 14:46:54 +0000

    - Tablet Portrait SChrift größer - Tablet Landscape. Map direkt in Standortdetailseite

--- trunk/MuellGuideMs/src/mawi/muellguidems/activities/StandortDetailsActivity.java
+++ trunk/MuellGuideMs/src/mawi/muellguidems/activities/StandortDetailsActivity.java
@@ -1,22 +1,35 @@
 package mawi.muellguidems.activities;
 
+import java.util.ArrayList;
+
 import mawi.muellguidems.parseobjects.Bezirk;
 import mawi.muellguidems.parseobjects.Entsorgungsart;
 import mawi.muellguidems.parseobjects.Standort;
 import mawi.muellguidems.util.DAO;
 import mawi.muellguidems.util.EntsorgungsartUtil;
+import mawi.muellguidems.util.MapUtils;
 import mawi.muellguidems.util.NetworkIdentifier;
 import mawi.muellguidems.util.NetworkIdentifier.NetworkCondition;
 import mawi.muellguidems.util.StringEnum;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class StandortDetailsActivity extends BaseActivity {
 
@@ -159,10 +172,48 @@ public class StandortDetailsActivity extends BaseActivity {
 						.showToastIfNecessary(netzwerkStatus);
 				return;
 			}
-			// Maps öffnen
-			Intent mapsIntent = new Intent(this, MapsActivity.class);
-			mapsIntent.putExtra("objectId", standort.getId());
-			startActivity(mapsIntent);
+
+			int display_mode = getResources().getConfiguration().orientation;
+			// is Tablet and ladscape?
+			if (isTablet(getApplicationContext()) && display_mode == 2) {
+				// Kopiert aus Maps Activity
+				GoogleMap googleMap = ((MapFragment) getFragmentManager()
+						.findFragmentById(R.id.map)).getMap();
+				ArrayList<MarkerOptions> allMarkers = MapUtils.getAllMakers(
+						null, standort.getId());
+
+				for (MarkerOptions markerOptions : allMarkers) {
+					googleMap.addMarker(markerOptions);
+				}
+
+				LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+				checkForServices(locationManager);
+
+				// Liest aktuelle Position mit Hilfe der MapUtils aus
+				LatLng zoomLocation = MapUtils
+						.getCurrentLocation(locationManager);
+
+				if (allMarkers.size() == 1) {
+					// Wenn nur eine Position zurückgegeben wird, soll auch nur
+					// auf
+					// diese Position gezoomt werden (z.B. bei Recyclinghöfen)
+					zoomLocation = allMarkers.get(0).getPosition();
+				}
+				CameraPosition cameraPosition = new CameraPosition.Builder()
+						.target(zoomLocation).zoom(12).build();
+
+				googleMap.animateCamera(CameraUpdateFactory
+						.newCameraPosition(cameraPosition));
+
+				googleMap.setMyLocationEnabled(true);
+				googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+			} else {
+				// Maps öffnen
+				Intent mapsIntent = new Intent(this, MapsActivity.class);
+				mapsIntent.putExtra("objectId", standort.getId());
+				startActivity(mapsIntent);
+			}
 		} else {
 			// AlertDialog für Feedback
 			AlertDialog.Builder builder = new AlertDialog.Builder(
@@ -189,4 +240,64 @@ public class StandortDetailsActivity extends BaseActivity {
 		}
 
 	}
+
+	public boolean isTablet(Context context) {
+		boolean xlarge = ((context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_XLARGE);
+		boolean large = ((context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_LARGE);
+		return (xlarge || large);
+	}
+
+	/**
+	 * Überprüft, welche Standort-Dienste aktiviert sind und gibt gegebenenfalls
+	 * ein Toast mit Hinweisen aus (Aus Maps Activity kopiert)
+	 * 
+	 * @param locationManager
+	 */
+	private void checkForServices(LocationManager locationManager) {
+		if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+				&& !locationManager
+						.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+			buildLocationSettingsAlert();
+
+		} else if (!locationManager
+				.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+			Toast.makeText(getBaseContext(), R.string.gps_gpsEinschalten,
+					Toast.LENGTH_LONG).show();
+		} else if (!locationManager
+				.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+			Toast.makeText(getBaseContext(),
+					R.string.gps_lokalisierungPerNetzwerk, Toast.LENGTH_LONG)
+					.show();
+		}
+	}
+
+	/**
+	 * Erzeugt ein Popup, wenn keine Einstellungen aktiviert sind (Aus Maps
+	 * Activity kopiert)
+	 */
+	private void buildLocationSettingsAlert() {
+		// AlertDialog für Feedback
+		AlertDialog.Builder builder = new AlertDialog.Builder(
+				StandortDetailsActivity.this);
+		builder.setMessage(R.string.gps_keineDiensteAktiviert);
+		// Hinzufügen der Buttons
+		builder.setNegativeButton(R.string.zurueck,
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						// User bricht denn Dialog ab
+					}
+				});
+		builder.setPositiveButton(R.string.gps_einstellungenAendern,
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						// Einstellungen aufrufen
+						startActivity(new Intent(
+								android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+					}
+				});
+		// Dialog anzeigen
+		AlertDialog dialog = builder.create();
+		dialog.show();
+	}
+
 }
