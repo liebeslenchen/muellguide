7cc5e0a7bd5528871b1ce3e6f302ddfbadab4f92
Rother.Lena@gmail.com <Rother.Lena@gmail.com@494df4b8-804f-c712-43b1-10418e7df321>
2015-01-13 09:54:35 +0000

    Flugmodus und Layout

--- MuellGuideMs/src/mawi/muellguidems/activities/InfoActivity.java
+++ MuellGuideMs/src/mawi/muellguidems/activities/InfoActivity.java
@@ -1,8 +1,9 @@
 package mawi.muellguidems.activities;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-public class InfoActivity extends BaseActivity {
+public class InfoActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
